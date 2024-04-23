1f584a03ec8dce1d4511a0f0d59ba36879d9a835
yazan <yazansbilal@gmail.com>
2024-04-23 23:51:56 +0300

    linkedList adding values in the middle.

--- src/main/kotlin/data_structures/linked_list/LinkedList.kt
+++ src/main/kotlin/data_structures/linked_list/LinkedList.kt
@@ -1,5 +1,7 @@
 package data_structures.linked_list
 
+import org.w3c.dom.NodeList
+
 class LinkedList<T:Any>{
     private var head :ListNode<T>? = null
     private var tail : ListNode<T>? = null
@@ -28,14 +30,54 @@ class LinkedList<T:Any>{
     //append adding to the end of Linked list
 
     fun append(value: T):LinkedList<T> {
-        if(isEmpty()){
+        if (isEmpty()) {
             push(value)
             return this
         }
-        tail!!.next = ListNode(value , null)
+        tail!!.next = ListNode(value, null)
         tail = tail!!.next
         size++
         return this
     }
 
+    // insert a value in a specific place :
+    fun insertNode(value:T,index:Int):LinkedList<T>{
+        var pointer  = head
+        if (isEmpty()){
+            push(value)
+        }
+        if (index >= size){
+            println("index out of bounds")
+            return this
+        }
+        for (i in 0..index){
+            pointer = pointer?.next
+            if ( i== index -1 ){
+                pointer!!.next = ListNode(value , pointer?.next )
+            }
+        }
+        return this
+    }
+
+    fun nodeAt(index:Int):ListNode<T> {
+        var currentNode = head
+        var currentIndex = 0
+        while (currentNode != null && currentIndex < index){
+            currentNode = currentNode.next
+            currentIndex++
+        }
+        return currentNode !!
+    }
+
+    fun insert(value:T , afterNode:ListNode<T>):ListNode<T>{
+        if (tail == afterNode){
+            append(value)
+            return tail !!
+        }
+        val newNode = ListNode(value = value, next = afterNode)
+        afterNode.next = newNode
+        size ++
+
+        return newNode
+    }
 }
