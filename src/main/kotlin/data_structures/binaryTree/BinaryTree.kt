a85220c8d0265eeb0e18eac002687fe79bf782b4
yazan <yazansbilal@gmail.com>
2024-04-26 20:30:36 +0300

    Implementing binaryTree using pointers

---
+++ src/main/kotlin/data_structures/binaryTree/BinaryTree.kt
@@ -0,0 +1,7 @@
+package data_structures.binaryTree
+
+class BinaryTree<T:Any> (
+    var root :TreeNode<T>? = null ,
+    var size:Int = 0
+) {
+}
---
+++ src/main/kotlin/data_structures/binaryTree/TreeNode.kt
@@ -0,0 +1,9 @@
+package data_structures.binaryTree
+
+class TreeNode<T:Any>(
+    var leftChild:TreeNode<T>? = null,
+    var rightChild:TreeNode<T>? = null ,
+    var value : T
+) {
+
+}
